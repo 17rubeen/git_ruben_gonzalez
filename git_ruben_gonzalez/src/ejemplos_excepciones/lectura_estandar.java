8847a365d4f8cf09d0fe108488f12d95f9e8efec
17rubeen <96436106+17rubeen@users.noreply.github.com>
2022-06-05 22:59:14 +0200

    Update lectura_estandar.java

--- git_ruben_gonzalez/src/ejemplos_excepciones/lectura_estandar.java
+++ git_ruben_gonzalez/src/ejemplos_excepciones/lectura_estandar.java
@@ -5,7 +5,7 @@ import java.io.*;
 class lectura_estandar {
 	public static void main(String args[]) throws IOException {
 		int caracter;
-		System.out.println("Escribe lo que quieras.Para terminar, pulsa ENTER");
+		System.out.println("Escribe lo que quieras.Para terminar, pulsa enterr");
 		System.out.println();
 		while ((caracter = System.in.read()) != 13)
 			System.out.println((char) caracter);
